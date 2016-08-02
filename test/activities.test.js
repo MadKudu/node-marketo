ad713c6e88a726149d65a4e621b5c9729f21e065
Adam Argo <adamargo@gmail.com>
2016-08-02 15:17:52 -0700

    adds listing of activity types

---
+++ test/activities.test.js
@@ -0,0 +1,17 @@
+var assert = require('assert'),
+    _ = require('lodash'),
+    marketo = require('./helper/connection');
+
+describe('Activities', function() {
+  describe('list activity types', function() {
+  	it('lists activity types', function(done){
+	    marketo.activities.getActivityTypes().then(function(resp) {
+	      var activity = resp.result[0];
+	      assert.equal(activity.id, 1);
+	      assert.equal(activity.name, 'Visit Webpage')
+	      assert(_.has(activity, 'primaryAttribute'));
+	      done();
+	    });
+    });
+  });
+})
